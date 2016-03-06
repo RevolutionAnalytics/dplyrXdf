d960c3e5ddb597061a7b5f0b686fd62116b84df9
Hong-Revo <hongooi@microsoft.com>
2016-03-07 04:45:51 +1100

    Persist + factorise.data.frame take 2

---
+++ R/factorise_df.R
@@ -0,0 +1,29 @@
+#' @include factorise_xdf.R
+
+#' The data frame method simply calls \code{factor} to convert the specified columns into factors.
+#' @rdname factorise
+#' @export
+factorise_.data.frame <- function(.data, ..., .dots)
+{
+    # add a method for data frames because merging with xdfs can get messy
+    dots <- lazyeval::all_dots(.dots, ..., all_named=TRUE)
+
+    # identify Revo-specific arguments
+    dots <- .rxArgs(dots)
+    rxArgs <- dots$rxArgs
+    exprs <- dots$exprs
+
+    grps <- groups(.data)
+    types <- sapply(.data, function(x) class(x)[1])
+    vars <- factorise_vars(types, exprs)
+
+    .data[] <- lapply(names(.data), function(n) {
+        x <- .data[[n]]
+        if(n %in% vars$blankArgs)
+            factor(x)
+        else if(n %in% names(vars$newlevelArgs))
+            factor(x, levels=as.character(vars$newlevelArgs[[n]]))
+        else x
+    })
+    .data
+}
---
+++ R/persist_tbl_xdf.R
@@ -0,0 +1,48 @@
+#' Copy an xdf tbl to a permanent storage location.
+#'
+#' @param data An xdf tbl
+#' @param outFile Character string giving the name of the output xdf file
+#' @param overwrite If the outfile already exists, should it be overwritten?
+#' @param deleteOldTbl Should the original tbl file be removed?
+#' @param ... Other arguments to \code{\link[RevoScaleR]{rxDataStep}}
+#'
+#' @details
+#' By default, the underlying data for an xdf tbl is saved as a file in the R temporary directory, and is managed by dplyrXdf. This can cause confusion when a tbl is reused by later dplyrXdf operations, which can overwrite or delete the data.
+#'
+#' The \code{persist} verb is a simple shell around \code{rxDataStep} that copies the data into a new xdf file at the location given by \code{outFile}. This ensures that the data will not be modified by dplyrXdf, and will be persistent beyond the end of the R session.
+#'
+#' @return
+#' An \code{RxXdfData} object pointing to the new xdf file.
+#'
+#' @seealso
+#' \code{\link{as.data.frame.RxXdfData}}, \code{\link[RevoScaleR]{rxDataStep}}
+#' @rdname persist
+#' @export
+persist <- function(data, ...)
+{
+    UseMethod("persist")
+}
+
+
+#' @rdname persist
+#' @export
+persist.tbl_xdf <- function(data, outFile, overwrite=TRUE, deleteOldTbl=FALSE, ...)
+{
+    if(file.exists(outFile) && overwrite)
+        warning(sprintf("output file '%s' already exists, will be overwritten", outFile))
+    inFile <- tblFile(data)
+    on.exit(if(deleteOldTbl && file.exists(inFile)) file.remove(infile))
+    rxDataStep(data, outFile=outFile, overwrite=overwrite, ...)
+}
+
+
+#' @details
+#' Calling this verb on a non-tbl data source (eg a raw \code{RxXdfData} object) will give a warning and return the data source unchanged.
+#'
+#' @rdname persist
+#' @export
+persist.RxFileData <- function(data, ...)
+{
+    warning("dataset is already persistent")
+    data
+}
