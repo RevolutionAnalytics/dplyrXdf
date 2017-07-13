32e8df604fb6a89f306f595a7365b2065266d6a1
hong-revo <hongooi@microsoft.com>
2017-07-13 23:35:18 +1000

    fixup execBy for Spark

--- R/execByUtils.R
+++ R/execByUtils.R
@@ -1,6 +1,6 @@
 execByCheck <- function(execLst)
 {
-    allOk <- all(sapply(execLst, "[[", "status") == "OK")
+    allOk <- all(sapply(execLst, function(x) x$status[[1]] == "OK"))
     if(!allOk)
         stop("Bad result from rxExecBy")
 }
